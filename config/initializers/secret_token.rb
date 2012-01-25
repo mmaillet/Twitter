55c1cab18fa6b1f48e83c854226cffc6bbd51c3d
Megane <megane.maillet@ingesup.com>
2012-01-25 17:04:26 +0100

    creation du projet twitter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '90cae81cd47f43f552d1754bd4d11c5c4fa9629f50aecab9442f7e41f90b928f18fb596b0ee6586818fd633c85b74cddfaa417152f507afc1a58e1ddc79cf35c'
